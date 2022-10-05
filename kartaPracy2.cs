e8678de62f33c9a498085eb2257bc3b944abf161
28-FW 2E <114576960+Kuguar2@users.noreply.github.com>
2022-10-05 10:59:12 +0200

    dodanie reszty zadań kartyPracy2.pdf

--- kartaPracy2.cs
+++ kartaPracy2.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KartaPracy2
+namespace kartaPracy2
 {
     internal class Program
     {
@@ -60,7 +60,7 @@ namespace KartaPracy2
                 Wyjście: TAK / NIE
 
             int waga = int.Parse(Console.ReadLine());
-            int limit = 20;
+            const int limit = 20;
             if (waga < limit)
             {
                 Console.WriteLine("tak");
@@ -71,9 +71,75 @@ namespace KartaPracy2
             }
             */
 
+            /* zad 5. Napisz program, który sprawdzi, czy trzecia z podanych licz mieści się między pierwszą a drugą. Weź pod
+                uwagę, że kolejność wpisywania dwóch pierwszych liczb może nie być zachowana.
+                Przykłady:
+                3 6 5 – TAK
+                4 8 9 – NIE
+                3 8 3 – NIE
+                6 3 5 – TAK
+                Wejście: a, b, c
+                Wyjście: TAK / NIE
 
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            if (c < b && c > a || c > b && c < a)
+            {
+                Console.WriteLine("tak");
+            }
+            else
+            {
+                Console.WriteLine("nie");
+            }
+            */
+            /* zad 6. Napisz program, który sprawdzi, czy liczba pierwsza p spełnia Małe Twierdzenie Fermata. Stosujemy
+                wersję MTF w formie: ”Jeśli p jest pierwsza to dla każdego a liczba a
+                p − a jest podzielna przez p”.
+                Wejście: a, p
+                Wyjście: TAK / NIE
+            int a = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+
+            if ((Math.Pow(a, p) - a) % p == 0)
+            {
+                Console.WriteLine("tak, spełnia");
+            }
+            else
+            {
+                Console.WriteLine("nie, nie spełnia");
+            }
+            */
+
+            /* zad 7. Bajtożabka osiowa oddaje tylko równe skoki o długości s wzdłuż osi. Napisz algorytm, który sprawdzi, czy
+                żabka pokona zadany dystans w trzech skokach. Jako punkt startu przyjmujemy wartość p, a jako punkt
+                końcowy punkt k, gdzie p, k, s >= 0
+                Przykłady:
+                0 100 40 – TAK
+                0 60 20 – TAK
+                10 50 15 – TAK
+                30 100 20 – NIE
+                Wejście: p, k, s
+                Wyjście: TAK / NIE
+                Dodatkowe dla chętnych: Podaj dodatkowo istotne Twoim zdaniem parametry rozwiązania w stylu ile
+                żabce zabrakło do pokonania dystansu, o ile przekroczyła, ile skoków by wystarczyło, ile skoków zabrakło
+                do celu, kolor żaby itp..
+
+            int p = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int s = int.Parse(Console.ReadLine());
+            if ((p + 3*s) >= k)
+            {
+                Console.WriteLine("tak, żaba da rade");
+            }
+            else
+            {
+                Console.WriteLine("nie, żaba nie da rady");
+            }
+            */
 
             Console.ReadKey();
         }
     }
 }
+
