107a7ee12669bf2626e5785e9d25766135fa219f
28-FW 2E <114576960+Kuguar2@users.noreply.github.com>
2022-10-05 12:16:58 +0200

    pierwsze zadanie z kartyPracy5.pdf

---
+++ KartaPracy5.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KartaPracy5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 1. (Easy) Stwórz funkcję, która obliczy sumę dwucyfrowych liczb pierwszych.
+            
+            
+            int suma = 0;
+            int dzielniki;
+            for (int i = 10; i < 100; i++)
+            {
+                dzielniki = 0;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        dzielniki++;
+                    }
+                }
+                if( dzielniki == 0)
+                    {
+                        suma = suma + i;
+                    }
+            }
+            Console.WriteLine(suma);
+            */
+            
+            
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
