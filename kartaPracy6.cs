98acd3e297cbdf50b777bdb30bae483af1f0c673
28-FW 2E <114576960+Kuguar2@users.noreply.github.com>
2022-12-13 23:13:52 +0100

    dodanie pierwszych 2 zadań z karty pracy 6

---
+++ kartaPracy6.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* zad 1
+            Console.WriteLine("Podaj 1-szą liczbę: ");
+            float a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj 2-gą liczbę: ");
+            float b = float.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj 3-cią liczbę: ");
+            float c = float.Parse(Console.ReadLine());
+            if (a - b == b - c || b - a == c - b)
+            {
+                Console.WriteLine("Ciąg jest arytmetyczny");
+            }
+            else
+            {
+                Console.WriteLine("Ciąg nie jest arytmetyczny");
+            }
+            if (b / a == c / b || a / b == b / c)
+            {
+                Console.WriteLine("Ciąg jest geometryczny");
+            }
+            else
+            {
+                Console.WriteLine("Ciąg nie jest geometryczny");
+            }
+            if (a > b && b > c)
+            {
+                Console.WriteLine("Ciąg jest malejący");
+            }
+            else
+            {
+                Console.WriteLine("Ciąg nie jest malejący");
+            }
+            if(a < b && b < c)
+            {
+                Console.WriteLine("ciąg jest rosnący");
+            }
+            else
+            {
+                Console.WriteLine("Ciąg nie jest rosnący");
+            }
+            */
+
+            /* zad 2
+            int suma = 0;
+            for(int i = 100; i < 1000; i++)
+            {
+                if(i % 8 == 0 && i % 16 != 0)
+                {
+                    suma += i;
+                    Console.WriteLine($"{i}");
+                }
+            }
+            Console.WriteLine($"suma = {suma}");
+            */
+
+
+            
+            Console.ReadKey();
+        }
+    }
+}
