ddb9adef8fd9cc9aab76d28164a91cbc79667fef
28-FW 2E <114576960+Kuguar2@users.noreply.github.com>
2022-12-14 12:17:03 +0100

    dodanie 6 zadań do kartyPracy6

--- kartyPracy/kartaPracy6.cs
+++ kartyPracy/kartaPracy6.cs
@@ -84,15 +84,93 @@ namespace ConsoleApp3
             Console.WriteLine($"{ile}");
             */
 
+            /* zad 4
             int ile = 0;
             for(int i = 10; i < 100; i++)
             {
                 if(i / 10 > i % 10 && i / 10 >= i % 10 * 2) {
                     ile += 1;
-                    Console.WriteLine($"{i}");
                 }
             }
             Console.WriteLine($"{ile}");
+            */
+
+            /* zad 5
+            int suma = 0, ile = 0;
+            for(int i = 100; i < 1000; i++)
+            {
+                if(i / 100 > ((i / 10) % 10) + ((i % 100) % 10)){
+                    ile += 1;
+                    suma += i;
+                }
+            }
+
+            Console.WriteLine($"ilość takich liczb to: {ile} a ich suma wynosi {suma}");
+            */
+
+            /* zad 6
+            int suma = 0,max = 0;
+            Console.WriteLine("Podaj ile liczb dwucyfrowych podzielnych przez 19 chcesz dodać: ");
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 10; i < 100; i++)
+            {
+                if(i % 19 == 0 && max < n)
+                {
+                    suma += i;
+                    Console.WriteLine(i);
+                    max += 1;
+                }
+            }
+            Console.WriteLine($"suma = {suma}");
+            */
+
+            /* zad 7
+            int suma = 0, max = 0;
+            Console.WriteLine("Podaj ile liczb dwucyfrowych podzielnych przez 37 chcesz dodać: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 100; i < 1000; i++)
+            {
+                if (i % 37 == 0 && max < n)
+                {
+                    suma += i;
+                    Console.WriteLine(i);
+                    max += 1;
+                }
+            }
+            Console.WriteLine($"suma = {suma}");
+            */
+
+            /* zad 8
+            int suma = 0;
+            Console.WriteLine("Podaj długość ciągu skaczącego: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= n * 3; i += 3)
+            {
+                if (i % 2 == 0)
+                {
+                    suma += i;
+                }
+                if(i % 2 != 0)
+                {
+                    suma += -i;
+                }
+            }
+            Console.Write($" suma = {suma}");
+            */
+
+            /* zad 9
+            int iloczyn = 1, liczba = 1;
+            Console.WriteLine("Podaj długość ciągu zakręconego: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i < n; i++)
+            {
+                liczba *= -2;
+                iloczyn *= liczba;
+            }
+            Console.Write($" iloczyn = {iloczyn}");
+            */
+
+
 
             Console.ReadKey();
         }
