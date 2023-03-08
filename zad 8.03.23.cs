00a1ed237f8f878a2ddae031fa36095c1e0fc6c6
28-FW 2E <114576960+Kuguar2@users.noreply.github.com>
2023-03-08 11:59:14 +0100

    Update zad 8.03.23.cs

--- zad 8.03.23.cs
+++ zad 8.03.23.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
@@ -11,12 +12,21 @@ namespace ConsoleApp1
     {
         public string Model { get; set; }
         public string Marka { get; set; }
+        public virtual void wyswietl()
+        {
+            Console.WriteLine("Marka:" + Marka + "Model: " + Model);
+        }
     }
     class Osoby {
         public string imie { get => imie; set => imie = value; }
     }
     class PC : Komputer {
         public string rodzajobudowy { get; set; }
+        public override void wyswietl()
+        {
+            base.wyswietl();
+            Console.WriteLine("Rodzaj obudowy: " + rodzajobudowy);
+        }
     }
     class Laptop : Komputer
     {
@@ -24,6 +34,11 @@ namespace ConsoleApp1
         public string kolorobudowy;
         public String getKolor() { return kolorobudowy; }
         public void setKolor(String kolorobudowy) { this.kolorobudowy = kolorobudowy; }
+        public override void wyswietl()
+        {
+            base.wyswietl();
+            Console.WriteLine("Długość przekątnej: " + dlugprzekatnej + "Kolor: " + kolorobudowy);
+        }
     }
     class Opiekun : Osoby 
     {
@@ -73,9 +88,50 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Komputer k1 = new Komputer();
-            k1.Model = Console.ReadLine();
-            Console.WriteLine(k1.Model);
+            // Prosty sposób
+
+            /*
+            Laptop l1 = new Laptop();
+            l1.Marka = Console.ReadLine();
+            l1.Model = Console.ReadLine();
+            l1.dlugprzekatnej = double.Parse(Console.ReadLine());
+            l1.setKolor(Console.ReadLine());
+            Console.WriteLine(l1.Marka + " " + l1.Model + " " + l1.dlugprzekatnej + " " + l1.getKolor());
+            */
+
+            // trudniejszy i kreatywniejszy sposób
+
+            Console.WriteLine("Co masz do naprawy?");
+            Console.WriteLine("1 - Komp; 2 - lap: ");
+            int wybor = int.Parse(Console.ReadLine());
+            switch (wybor)
+            {
+                case 1:
+                    {
+                        PC pc = new PC();
+                        pc.Marka = Console.ReadLine();
+                        pc.Model = Console.ReadLine();
+                        pc.rodzajobudowy = Console.ReadLine();
+                        pc.wyswietl();
+
+                        break;
+                    }
+                case 2:
+                    {
+                        Laptop l1 = new Laptop();
+                        l1.Marka = Console.ReadLine();
+                        l1.Model = Console.ReadLine();
+                        l1.dlugprzekatnej = double.Parse(Console.ReadLine());
+                        l1.setKolor(Console.ReadLine());
+                        l1.wyswietl();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("miałeś podać 1 lub 2");
+                        break;
+                    }
+            }
             Console.ReadKey();
         }
     }
